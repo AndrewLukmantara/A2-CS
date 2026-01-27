e058f2a62da7d6b5fc4b222c231a10fd4896d000
Sheeranui <97437308+Sheeranui@users.noreply.github.com>
2026-01-27 16:36:49 +0700

    1/27/2026

--- ADTs/LinkedList.py
+++ ADTs/LinkedList.py
@@ -1,52 +1,46 @@
-List = [[0 for _ in range(2)] for _ in range(10)]
-HeapPointer = 0
-StartPointer = -1
+# Linked List in Python
 
-for i in range(10):
+List = [[0 for _ in range(2)] for _ in range(10)] # ARRAY[0:9, 0:1] OF INTEGER
+HeapPointer = 0 # INTEGER
+StartPointer = -1 # INTEGER
+
+for i in range(10): # i : INTEGER
     List[i][1] = i + 1
 
-List[9][1] = StartPointer
+List[9][1] = -1
 
 print(List)
+print(f"Heap Pointer Value : {HeapPointer}\nStart Pointer Value : {StartPointer}")
 
 def addNode_End(data):
 
     global StartPointer, HeapPointer, List
 
-    if StartPointer == -1:
-        List[HeapPointer][0] = data
-        StartPointer = HeapPointer
-        HeapPointer += 1
-        print("Success")
+    if HeapPointer == -1:
+        print("List is full")
+        return False
 
-    elif HeapPointer == 9:
-        print("Unsuccessful")
+    # creating node
 
-    else:
-        List[HeapPointer][0] = data
-        HeapPointer += 1
-        print("Success")
+    newNode = HeapPointer # newNode : INTEGER
+    HeapPointer = List[HeapPointer][1]
+    List[newNode][0] = data
+    List[newNode][1] = -1 
 
-def addNode_Middle(data, position):
+    # adding node
 
-    global StartPointer, HeapPointer, List
-
-    if position > 0 and position < HeapPointer - 1:
-        List[HeapPointer][0] = data
-        List[position][1] = HeapPointer
-        List[HeapPointer][1] = position + 1
-        HeapPointer += 1
-        print("Success")
-    else:
-        print("Unsuccessful")
+    if StartPointer == -1:  # list is empty
+        StartPointer = newNode
+    else:  # list is not empty
+        current = StartPointer # current : INTEGER
+        while List[current][1] != -1:
+            current = List[current][1]
+        List[current][1] = newNode
 
 
 addNode_End("Cookie")
 addNode_End("Orange")
 addNode_End("Muffin")
-addNode_End("Tick")
-print(f"Heap Pointer Value : {HeapPointer}\nStart Pointer Value : {StartPointer}")
+
+print(f"\n \nHeap Pointer Value : {HeapPointer}\nStart Pointer Value : {StartPointer}")
 print(List)
-addNode_Middle("Goggles", 2)
-print(List)
-print(f"Heap Pointer Value : {HeapPointer}\nStart Pointer Value : {StartPointer}")
